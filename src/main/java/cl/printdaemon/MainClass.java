be3782229905ffe705ae09519f35e5edcb7dc1b2
Jorge Urzua <jjjurzua@hotmail.com>
2016-11-13 17:18:26 +0100

    Adds rights

--- src/main/java/cl/printdaemon/MainClass.java
+++ src/main/java/cl/printdaemon/MainClass.java
@@ -4,22 +4,21 @@ import cl.printdaemon.utils.WmicPrintJobOutput;
 
 public class MainClass {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
 		
 		String[] cmd = {"wmic printjob get jobid, document, jobstatus"};
 		
-		
-	    try {
-	    	
-	    	Process pb = Runtime.getRuntime().exec(cmd);
-		    pb.waitFor();
-		    WmicPrintJobOutput output = new WmicPrintJobOutput(pb);
-		    
-		} catch (Exception e) {
-			e.printStackTrace();
+		while(true){
+            Thread.sleep(500);
+			try {
+		    	System.out.println("*");
+		    	Process pb = Runtime.getRuntime().exec(cmd);
+			    pb.waitFor();
+			    WmicPrintJobOutput output = new WmicPrintJobOutput(pb);
+			    
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		
-		
-		System.exit(0);
 	}
 }
