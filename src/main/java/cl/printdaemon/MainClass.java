d1e3d744cec1e8709c3f396d1378a3327a832ef8
Jorge Urzua <jjjurzua@hotmail.com>
2016-11-13 18:25:34 +0100

    Adds methods to get userName and ComputerName from the windows system

--- src/main/java/cl/printdaemon/MainClass.java
+++ src/main/java/cl/printdaemon/MainClass.java
@@ -6,8 +6,20 @@ public class MainClass {
 
 	public static void main(String[] args) throws InterruptedException{
 		
-		String[] cmd = {"wmic", "printjob", "get"};
+		String username = System.getProperty("user.name");
 		
+		System.out.println(username);
+		
+		try {
+			java.net.InetAddress localMachine = java.net.InetAddress.getLocalHost();
+			System.out.println("Hostname of local machine: " + localMachine.getHostName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		String[] cmd = {"wmic", "printjob", "get"};
+		/*
 		while(true){
             Thread.sleep(500);
 			try {
@@ -19,6 +31,6 @@ public class MainClass {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}
+		}*/
 	}
 }
